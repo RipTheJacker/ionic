ad11fedfb12357fb5de18bae63a39be5966be435
Ionitron <hi@ionicframework.com>
2014-05-14 23:00:57 +0000

    finalize-release: v1.0.0-beta.5c "darmstadtium-dingo"

--- release/js/ionic-angular.js
+++ release/js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.5b
+ * Ionic, v1.0.0-beta.5c
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- release/js/ionic.js
+++ release/js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.5b
+ * Ionic, v1.0.0-beta.5c
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.5b'
+  version: '1.0.0-beta.5c'
 };
 
 (function(ionic) {
